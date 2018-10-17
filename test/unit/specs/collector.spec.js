626f2366f588b37f60dd3a5ac056f1a863d50d4b
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-10-17 15:58:17 +0300

    add one more test into collector.spec - tests

--- test/unit/specs/collector.spec.js
+++ test/unit/specs/collector.spec.js
@@ -4,10 +4,12 @@ const { collector } = require('../../e2e/utils/page.collector');
 const expectedJson = require('../data/expected.pages.json');
 const pathToExistingFile = './test/unit/data/expected.pages.json';
 const { expect } = require('chai');
+const fs = require('fs');
 const pageTemplatesDir = './test/unit/data/page-snippets/pages';
 const destDir = './test/unit/source'
 const nonExistingFolder = './nonExistentDir';
-const emptyDir = './test/unit/data/emptyDir'
+const emptyDir = './test/unit/data/emptyDir';
+const testJsFile = `${pageTemplatesDir}/empty.js`;
 
 
 describe('Unit tests of collector functionality', () => {
@@ -18,18 +20,20 @@ describe('Unit tests of collector functionality', () => {
     });
 
     it('should throw an error if a directory with pages does not exist', () => {
-        expect(function () { collector(nonExistingFolder, destDir) }).to.throw(Error);
+        expect(() => { collector(nonExistingFolder, destDir) }).to.throw(Error);
     })
 
     it('should throw an error if a passed path is not a directory', () => {
-        expect(function () { collector(pathToExistingFile, destDir) }).to.throw(Error, `The passed source path [${pathToExistingFile}] is not a directory.`);
+        expect(() => { collector(pathToExistingFile, destDir) }).to.throw(Error, `The passed source path [${pathToExistingFile}] is not a directory.`);
     })
 
     it('should throw an error if the directory is empty', () => {
-        expect(function () { collector(emptyDir, destDir) }).to.throw(Error, `Passed directory [${emptyDir}] is empty.`);
+        expect(() => { collector(emptyDir, destDir) }).to.throw(Error, `Passed directory [${emptyDir}] is empty.`);
     })
 
-    // it('should throw an error if an element does not have a selector', () => {
-    //     expect(function () { collector.getAllPages('.' + incorrectDir) }).to.throw(Error, 'Object { name: \'page\',\n  url: \'\',\n  children: { Header: { ref: \'./components/header/header.json\' } } } doesn\'t have a selector!');
-    // })
+    it('should not throw an error and should ignore no json files', () => {
+        fs.writeFileSync(testJsFile, '', 'utf8');
+        expect(() => { collector(pageTemplatesDir, destDir) }).to.not.throw(Error);
+        fs.unlinkSync(testJsFile);
+    })
 });
