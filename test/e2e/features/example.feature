13ffd3a6d112f3d91f886be576d8d1c89e22e576
Dzmitry_Karneyenka <Dzmitry_Karneyenka@epam.com>
2018-10-19 12:55:57 +0300

    The function getElement works appropriately

--- test/e2e/features/example.feature
+++ test/e2e/features/example.feature
@@ -6,5 +6,5 @@ Background: Background
    Given I am on 'https://www.bellagio.com/en.html' url
 
    Scenario: Scenario name
-   When I click on 'header > top header > guest services' 
+   When I click 'header > top header > guest services' 
    And I wait for '5' seconds
--- test/e2e/utils/tester.js
+++ test/e2e/utils/tester.js
@@ -8,24 +8,19 @@ async function getPageObject() {
     if (!pages[appenderUrl]) {
         throw new Error(`The framework has not included suitable page-object for this url [${appenderUrl}]`);
     } else {
-        const page = pages[appenderUrl][Object.keys(pages[appenderUrl]).find(key => /page/i.test(key))];
-        // console.log(page);
-        return page;
+        return pages[appenderUrl][Object.keys(pages[appenderUrl]).find(key => /page/i.test(key))];
     }
-    // return pages["en/entertainment.html"]["entertainment page"];
 }
 
 async function getElement(string) {
     const po = await getPageObject();
     await browser.wait(ec.presenceOf(element(by.css(po.selector))), 5000);
     const baseElement = element(by.css(po.selector));
-    // console.log(await baseElement.isPresent()) //'baseElement' //
     return /\s?\>\s?/.test(string) ? getElementFromChain(baseElement, po, string) : getElementFromString(baseElement, po, string);
 };
 
 function getRegex(string) {
     let regexes = [/^#\d+/, /#\d+$/, /^#first/, /#first$/, /^#second/, /#second$/, /^#last/, /#last$/];
-    // console.log(regexes.splice(2,1));
     regexes = regexes.filter(regex => regex.test(string.trim()));
     if (regexes.length > 1) {
         throw new Error(`There is more than one option in [${string}].`);
@@ -34,13 +29,12 @@ function getRegex(string) {
     }
 }
 
-function getElementFromChain(baseElement, po, chain) {
+async function getElementFromChain(baseElement, po, chain) {
     const names = chain.split(/\s?\>\s?/);
-    return names.forEach(name=> {
-        getElementFromString(baseElement, po, name)
-        .then((element)=>{baseElement = element; });
-        po = po.children[name.trim()];
-    });
+    for (let i = 0; i < names.length; i++) {
+        baseElement = await  getElementFromString(baseElement, po, names[i]);
+        po = po.children[names[i].trim()];
+      }
     return baseElement;
 }
 
@@ -50,17 +44,12 @@ function getElementFromString(baseElement, po, string) {
 }
 
 
-async function getElementByString(baseElement, po, string) {
-    console.log('|' + string.trim() + '|');
+function getElementByString(baseElement, po, string) {
     po = po['children'][string.trim()];
-    console.log(po.selector);
     if (!po) {
         throw new Error(`There is no [children] property in object [${po}]`);
     } else {
-        console.log('BAseElement: ['+await baseElement.isPresent()+']')
-        const elem = await  baseElement.element(by.css(po.selector));
-        console.log(await elem.isPresent());
-        return elem;
+        return baseElement.element(by.css(po.selector));
     }
 }
 
